0f0a6df0f297cd97558ca08ab2111c99314eea57
101 <elaine9210@gmail.com>
2024-12-06 16:47:42 +0800

    嘗試解決無法註冊 bug

--- ShoppingWeb/js/register.js
+++ ShoppingWeb/js/register.js
@@ -16,12 +16,11 @@ document.getElementById('registerForm').addEventListener('submit', async (event)
 
     try {
         // 發送請求
-        const response = await fetch('/register', {                 // 使用 fetch API 發送 HTTP POST 請求到後端 /register 路徑
+        const response = await fetch('http://127.0.0.1:5000/register', {                 // 使用 fetch API 發送 HTTP POST 請求到後端 /register 路徑
             method: 'POST',                                         // 指定請求方法為 POST
             headers: { 'Content-Type': 'application/json' },        // 告訴伺服器這是 JSON 格式的資料
             body: JSON.stringify({ username, email, password }),    // 將 username、email 和 password 包裝成 JSON 字符串傳送
         });
-/*
         const result = await response.json();   // 使用 response.json() 解析後端回應的 JSON 資料
         alert(response);
         if (response.ok) {                      // 成功處理
@@ -30,7 +29,6 @@ document.getElementById('registerForm').addEventListener('submit', async (event)
         } else {                                // 錯誤處理
             alert(result.error);
         }
-            */
     }
     // 錯誤捕獲 
     catch (error) {
